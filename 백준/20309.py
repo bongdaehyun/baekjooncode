d6c1edc7bbf1c0f1069cf698bf877338efe98c34
봉대현 <pkjeogus@gmail.com>
2021-01-21 22:24:47 +0900

    수정

--- 백준/20309.py
+++ 백준/20309.py
@@ -5,8 +5,7 @@ n_list=list(map(int,sys.stdin.readline().split()))
 
 check=True
 for i in range(N):
-    print(i % 2, n_list[i])
     if i%2==n_list[i]%2:
         check=False
-        break
-print("Yes" if check else "NO")
+
+print("YES" if check else "NO")
