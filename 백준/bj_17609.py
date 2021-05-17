08beade9e4d773edb4cec8392154565cb6f8964d
봉대현 <pkjeogus@gmail.com>
2021-05-18 00:00:00 +0900

    수정

--- 백준/bj_17609.py
+++ 백준/bj_17609.py
@@ -1,36 +1,36 @@
 import sys
+
+#회문인지 판단
+def firstcheck(word,left,right):
+    while left< right:
+        if word[left]==word[right]:
+            left+=1
+            right-=1
+        else:
+            a=twocheck(word,left+1,right)
+            b=twocheck(word,left,right-1)
+            if a or b:
+                return 1
+            else:
+                return 2
+    return 0
+
+#유사회문인지 아닌지 판단
+def twocheck(word,left,right):
+    while left<right:
+        if word[left] == word[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
+
 n=int(sys.stdin.readline());
 str_list=[]
 for i in range(n):
-    str_list.append([i for i in sys.stdin.readline().rstrip()])
-
-ans=[]
-for s in str_list:
-    check=False #2번들어가는 걸 방지 ex) flag
-    flag=False #유사회문 판단
+    word=sys.stdin.readline().rstrip()
     left=0
-    right=len(s)-1
-    while left<right:
-        if s[left]!=s[right]:
-           if flag:
-               ans.append(2)
-               check=True
-               break
-           else:
-               flag=True
-               if s[left + 1] == s[right]:
-                   left+=1
-               elif s[left] == s[right-1]:
-                   right-=1
-               else:
-                   ans.append(2)
-                   check=True
-                   break
-        left+=1
-        right-=1
-    if not check and flag:
-        ans.append(1)
-    elif not check and not flag:
-        ans.append(0)
-for i in ans:
-    print(i)
+    right=len(word)-1
+    ans=firstcheck(word,left,right)
+    print(ans)
+
